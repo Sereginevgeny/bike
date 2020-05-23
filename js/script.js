f2bdd560b854a0b964f9637634fb61c5ebefebec
Sereginevgeny <64419111+Sereginevgeny@users.noreply.github.com>
2020-05-23 16:05:52 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@
 
 // alert(admin);
 
-let yourName = prompt ("Как вас зовут?");
-if (yourName == ""){
+let yourName = prompt ("Как вас зовут?", "");
+if (yourName == " "){
 alert ("Здравствуйте " + yourName + ". Добро пожаловать на наш сайт!");
 }else { alert ("Введите свое имя!")
 }
